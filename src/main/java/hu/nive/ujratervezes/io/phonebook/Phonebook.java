88dfcee5f9d9f2ebf8f301b04538f2793b2bc376
meridian74 <meridian@freemail.hu>
2021-04-29 12:19:27 +0200

    all test are done

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,31 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output) {
+        try {
+            if (contacts == null) throw new IllegalArgumentException();
+
+            File file = new File(output);
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+
+            for (Map.Entry<String, String> entry : contacts.entrySet()) {
+                bufferedWriter.write(entry.getKey() + ": " + entry.getValue());
+                bufferedWriter.newLine();
+            }
+
+            bufferedWriter.flush();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+
+        } catch (NullPointerException ex) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
